4dccbf1215f575240176ae600f1b487e52f5fe1c
minnalnh <58827713+minnalnh@users.noreply.github.com>
2022-05-04 15:39:26 +0200

    Add files via upload

---
+++ laboration-1/app.js
@@ -0,0 +1,53 @@
+//Laboration 1
+
+app.get('/', function(request, response) {
+
+    var startsida = "index.html";
+    
+    const express = require('express');
+    const jsDOM = require('jsdom');
+    const fs = require('fs');
+
+    let app = express();
+
+    app.use('/public', express.static(__dirname + '/staic'));
+
+    app.use(express.urlencoded( {extended : true}));
+
+    app.listen(81, function() {
+        console.log('Servern är igång!');
+    });
+
+    app.get('/', function(request, response){
+        console.log('En utskrift från get...');
+        response.sendFile(__dirname + 'index.html');
+    });
+
+    app.post('/', function(request, response) {
+        console.log('En utskrift från post...');
+        
+        fs.readFile(__dirname + 'index.html', function(error, data){
+            if(error){
+                console.log('fel');
+            }
+            else{
+                let htmlCode = data;
+
+                let serverDOM = new jsDOM.JSDOM(htmlCode);
+
+                let section = serverDOM.window.document.querySelector('section');
+
+                htmlCode = serverDOM.serialize();
+
+                response.send(htmlCode);
+            
+            }
+        })
+    
+    
+    });
+// response.sendFile(__dirname + '/static/html/index.html);
+
+});
+
+
